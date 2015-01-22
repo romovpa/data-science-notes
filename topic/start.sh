fcbcaff0e8dc78570bd39577654ac8c6ffea7520
Petr Romov <romovpa@yandex-team.ru>
2015-01-23 02:08:16 +0300

    topic: notebooks updated

--- topic/start.sh
+++ topic/start.sh
@@ -3,4 +3,4 @@
 PYTHONPATH=/Users/romovpa/workspace/bigartm/src/python/examples/src/python:$PYTHONPATH
 ARTM_SHARED_LIBRARY=/Users/romovpa/workspace/bigartm/src/python/examples/build/src/artm/libartm.dylib
 
-ipython notebook --port 4000 --no-browser 
+ipython notebook --port 4000 --no-browser --ip \*
